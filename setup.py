12d25b20f22303c8e6a77d2ea53178b5ce118e13
Isaac To <isaacto@gmail.com>
2020-08-23 20:57:58 +0800

    Set version number to 0.4.1
    Signed-off-by: Isaac To <isaacto@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='makefilemenu',
-    version='0.4',
+    version='0.4.1',
     python_requires='~=3.5',
     author='Isaac To',
     author_email='isaac.to@gmail.com',
